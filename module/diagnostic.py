27681dce184759c60ae134dc2e26c8f72e178a3e
Gabriel Oest <bieloest@gmail.com>
2019-02-01 22:27:42 +0000

    Refactoringo

--- module/diagnostic.py
+++ module/diagnostic.py
@@ -65,14 +65,15 @@ high_messages_unacknowledged_queues(queues, conditions)
 nodes = list(rmq_utils.get_nodes().json())
 
 
-def alert_file_description(node):
+def alert_file_description(node, conditions):
     fd_used = node['fd_used']
     fd_total = node['fd_total']
     fd_percent = (fd_used * 100) / fd_total
     fd_result = 'File Descriptors Alert '
-    if(fd_percent > 90 and fd_percent < 98):
+    if(fd_percent > conditions['file_descriptors_used_warn'] and
+       fd_percent < conditions['file_descriptors_used_critical']):
         fd_result += 'Warning: {}'.format(fd_percent)
-    elif(fd_percent > 98):
+    elif(fd_percent > conditions['file_descriptors_used_critical']):
         fd_result += 'Critical: {}'.format(fd_percent)
     else:
         fd_result += 'OK: {}'.format(fd_percent)
@@ -80,14 +81,15 @@ def alert_file_description(node):
     print(fd_result)
 
 
-def alert_files_description_as_sockets(node):
+def alert_files_description_as_sockets(node, conditions):
     sd_used = node['sockets_used']
     sd_total = node['sockets_total']
     sd_percent = (sd_used * 100) / sd_total
     sd_result = 'File Descriptors as Sockets Alert '
-    if(sd_percent > 90 and sd_percent < 98):
+    if(sd_percent > conditions['file_descriptors_used_as_sockets_warn'] and
+       sd_percent < conditions['file_descriptors_used_as_sockets_critical']):
         sd_result += 'Warning: {}'.format(sd_percent)
-    elif(sd_percent > 98):
+    elif(sd_percent > conditions['file_descriptors_used_as_sockets_critical']):
         sd_result += 'Critical: {}'.format(sd_percent)
     else:
         sd_result += 'OK: {}'.format(sd_percent)
@@ -95,14 +97,15 @@ def alert_files_description_as_sockets(node):
     print(sd_result)
 
 
-def alert_disk_free(node):
+def alert_disk_free(node, conditions):
     disk_free = node['disk_free']
     disk_free_limit = node['disk_free_limit']
     disk_usage_percent = (disk_free_limit * 100) / disk_free
     sd_result = 'Disk Free Alert '
-    if(disk_usage_percent > 90 and disk_usage_percent < 98):
+    if(disk_usage_percent > conditions['disk_space_used_warn'] and
+       disk_usage_percent < conditions['disk_space_used_critical']):
         sd_result += 'Warning: {}'.format(disk_usage_percent)
-    elif(disk_usage_percent > 98):
+    elif(disk_usage_percent > conditions['disk_space_used_critical']):
         sd_result += 'Critical: {}'.format(disk_usage_percent)
     else:
         sd_result += 'OK: {}'.format(disk_usage_percent)
@@ -110,14 +113,15 @@ def alert_disk_free(node):
     print(sd_result)
 
 
-def alert_mem_free(node):
+def alert_mem_free(node, conditions):
     mem_used = node['mem_used']
     mem_limit = node['mem_limit']
     mem_usage_percent = (mem_used * 100) / mem_limit
     mem_result = 'Mem Free Alert '
-    if(mem_usage_percent > 90 and mem_usage_percent < 98):
+    if(mem_usage_percent > conditions['memory_used_warn'] and
+       mem_usage_percent < conditions['memory_used_critical']):
         mem_result += 'Warning: {}'.format(mem_usage_percent)
-    elif(mem_usage_percent > 98):
+    elif(mem_usage_percent > conditions['memory_used_critical']):
         mem_result += 'Critical: {}'.format(mem_usage_percent)
     else:
         mem_result += 'OK: {}'.format(mem_usage_percent)
@@ -125,14 +129,15 @@ def alert_mem_free(node):
     print(mem_result)
 
 
-def alert_erlang_process(node):
+def alert_erlang_process(node, conditions):
     proc_used = node['proc_used']
     proc_total = node['proc_total']
     proc_usage_percent = (proc_used * 100) / proc_total
     proc_result = 'Erlang processes used Alert '
-    if(proc_usage_percent > 90 and proc_usage_percent < 98):
+    if(proc_usage_percent > conditions['erlang_process_warn'] and
+       proc_usage_percent < conditions['erlang_process_critical']):
         proc_result += 'Warning: {}'.format(proc_usage_percent)
-    elif(proc_usage_percent > 98):
+    elif(proc_usage_percent > conditions['erlang_process_critical']):
         proc_result += 'Critical: {}'.format(proc_usage_percent)
     else:
         proc_result += 'OK: {}'.format(proc_usage_percent)
@@ -142,8 +147,8 @@ def alert_erlang_process(node):
 
 for node in nodes:
     print('Node {} metrics: '.format(node['name']))
-    alert_file_description(node)
-    alert_files_description_as_sockets(node)
-    alert_disk_free(node)
-    alert_mem_free(node)
-    alert_erlang_process(node)
+    alert_file_description(node, conditions)
+    alert_files_description_as_sockets(node, conditions)
+    alert_disk_free(node, conditions)
+    alert_mem_free(node, conditions)
+    alert_erlang_process(node, conditions)
